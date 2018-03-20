6ba8ad642fe2f1c52f8d79db51d7aa05bd7c95da
Thomas Fanninger <thomas@fanninger.at>
2018-03-20 20:44:20 +0100

    Bugfix Cron

--- root/etc/cont-init.d/90_configure_php.sh
+++ root/etc/cont-init.d/90_configure_php.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/with-contenv bash
 
 mkdir -p /conf
-mkdir -p /config/phpext
 
 dockerize -template /app/start_php.sh:/conf/start_php.sh
 dockerize -no-overwrite -template /app/php-fpm_www.conf:/config/php-fpm_www.conf
@@ -9,11 +8,6 @@ dockerize -no-overwrite -template /app/php.ini:/config/php.ini
 
 ln -s /config/php-fpm_www.conf /etc/php7/php-fpm.d/www.conf
 ln -s /config/php.ini /etc/php7/php.ini
-
-# add
-for file in /config/phpext/*.ini; do
-  cat /config/phpext/$(basename "$file") > /etc/php7/conf.d/$(basename "$file")
-done
 
 chmod +x /conf/start_php.sh
 chown -R app:users /conf
