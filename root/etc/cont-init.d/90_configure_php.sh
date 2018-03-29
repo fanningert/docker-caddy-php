03114cf277754e12da7cb715d2f2aaeae5112062
Thomas Fanninger <thomas@fanninger.at>
2018-03-29 23:39:11 +0200

    Bugfix

--- root/etc/cont-init.d/90_configure_php.sh
+++ root/etc/cont-init.d/90_configure_php.sh
@@ -2,13 +2,13 @@
 
 mkdir -p /conf
 
-dockerize -template /app/start_php.sh:/conf/start_php.sh
-dockerize -no-overwrite -template /app/php-fpm_www.conf:/config/php-fpm_www.conf
-dockerize -no-overwrite -template /app/php.ini:/config/php.ini
+dockerize -template /app/start_php.sh:/config/start_php.sh
+dockerize -no-overwrite -template /app/php-fpm_www.conf:/conf/php-fpm_www.conf
+dockerize -no-overwrite -template /app/php.ini:/conf/php.ini
 
-ln -s /config/php-fpm_www.conf /etc/php7/php-fpm.d/www.conf
-ln -s /config/php.ini /etc/php7/php.ini
+ln -s /conf/php-fpm_www.conf /etc/php7/php-fpm.d/www.conf
+ln -s /conf/php.ini /etc/php7/php.ini
 
-chmod +x /conf/start_php.sh
+chmod +x /config/start_php.sh
 chown -R app:users /conf
 chown -R app:users /config
